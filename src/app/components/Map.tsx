c593c34af2bd803bd8c0ed1b77210210b10c4e78
Wim426F <wim.boone@outlook.com>
2025-04-08 21:16:06 +0200

    Fix Leaflet TypeScript error in Map.tsx

--- src/app/components/Map.tsx
+++ src/app/components/Map.tsx
@@ -1,16 +1,26 @@
 "use client";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css"; // Should work after PostCSS fix
+import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 
-delete L.Icon.Default.prototype._getIconUrl;
-L.Icon.Default.mergeOptions({
+// Define custom icon without modifying prototype
+const customIcon = new L.Icon({
   iconRetinaUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
   iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
   shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
 });
 
-type Aircraft = { reg: string; lat: number; lon: number; details: string };
+type Aircraft = {
+  reg: string;
+  lat: number;
+  lon: number;
+  details: string;
+};
+
 export default function TrackerMap({ flying }: { flying: Aircraft[] }) {
   return (
     <MapContainer center={[52.2958, 5.6177]} zoom={8} style={{ height: "100%", width: "100%" }}>
@@ -19,7 +29,7 @@ export default function TrackerMap({ flying }: { flying: Aircraft[] }) {
         attribution='© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
       />
       {flying.map((aircraft) => (
-        <Marker key={aircraft.reg} position={[aircraft.lat, aircraft.lon]}>
+        <Marker key={aircraft.reg} position={[aircraft.lat, aircraft.lon]} icon={customIcon}>
           <Popup>{aircraft.reg} - {aircraft.details}</Popup>
         </Marker>
       ))}
